1af98f56a33c01e97251ab27ad63a719314cb0ba
Ariel Alfonso Triana Pérez <61637781+ArielTriana@users.noreply.github.com>
2021-11-23 11:45:00 -0500

    Update test_heightmap_generation.py

--- test/test_heightmap_generation.py
+++ test/test_heightmap_generation.py
@@ -9,11 +9,11 @@ LIST_1 = array([[0.3, 0.4, 0.32], [0.9, 0.75, 0.01], [0.875, 0.2, 0.3]])
 
 def test_fit_function():
     h = HeightMap.build_from_map(LIST_1)
-    g = GAT_Generator(0.8,(3,3),sea_level=0.45)
+    g = GAT_Generator(0.8, (3, 3), sea_level=0.45)
 
     fit = g.fit_func(h)
 
-    assert fit == 3/9
+    assert fit == 3 / 9
 
 
 def test_generator_big_percentage():
@@ -31,7 +31,7 @@ def test_generator_small_percentage():
     tol = 0.03
     per = 0.05
     g = GAT_Generator(0.05, (10, 10), tol=tol)
-    
+
     map = g()
 
     fit = g.fit_func(map)
