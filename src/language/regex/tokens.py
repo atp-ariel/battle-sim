00625a395c6431bf326db15c2c31978d259ca0be
ArielTriana <usich37@gmail.com>
2022-01-14 11:54:37 -0500

    Implement lexer

---
+++ src/language/regex/tokens.py
@@ -0,0 +1,229 @@
+from abc import ABCMeta
+
+
+
+class Token(metaclass=ABCMeta):
+    @classmethod
+    def get_token_class(cls, c: str):
+        if c not in token_dict.keys():
+            raise ValueError()
+        return token_dict[c]
+
+
+class ElementToken(Token):
+    def __init__(self, char):
+        super().__init__()
+        self.type = 'element'
+        self.char = char
+
+
+class WildcardToken(Token):
+    def __init__(self, wildcard_ch):
+        super().__init__()
+        self.type = 'wildcard'
+        self.wildcard_ch = wildcard_ch
+        self.char = wildcard_ch
+
+
+class SpaceToken(Token):
+    def __init__(self, space_ch) -> None:
+        super().__init__()
+        self.type = 'space'
+        self.space_char = space_ch
+        self.char = space_ch
+
+
+class Wildcard(WildcardToken):
+    def __init__(self):
+        super().__init__(wildcard_ch='.')
+
+
+class StartToken(Token):
+    def __init__(self, start_ch):
+        super().__init__()
+        self.type = 'start'
+        self.start_ch = start_ch
+        self.char = start_ch
+
+
+class Start(StartToken):
+    def __init__(self):
+        super().__init__('~')
+
+
+class EndToken(Token):
+    def __init__(self, end_ch):
+        super().__init__()
+        self.type = 'end'
+        self.end_ch = end_ch
+        self.char = end_ch
+
+
+class End(EndToken):
+    def __init__(self):
+        super().__init__('$')
+
+
+class Escape(Token):
+    def __init__(self):
+        super().__init__()
+        self.type = 'escape'
+        self.escape_char = '\\'
+        self.char = '\\'
+
+
+class Comma(Token):
+    def __init__(self):
+        super().__init__()
+        self.type = 'comma'
+        self.char = ','
+
+
+class Parenthesis(Token):
+    def __init__(self, side):
+        super().__init__()
+        self.type = 'parenthesis'
+        self.side = side
+
+
+class LeftParenthesis(Parenthesis):
+    def __init__(self):
+        super().__init__(side='L')
+        self.char = '('
+
+
+class RightParenthesis(Parenthesis):
+    def __init__(self):
+        super().__init__(side='R')
+        self.char = ')'
+
+
+class CurlyBrace(Token):
+    def __init__(self, side):
+        super().__init__()
+        self.type = 'curly'
+        self.side = side
+
+
+class LeftCurlyBrace(CurlyBrace):
+    def __init__(self):
+        super().__init__(side='L')
+        self.char = '{'
+
+
+class RightCurlyBrace(CurlyBrace):
+    def __init__(self):
+        super().__init__(side='R')
+        self.char = '}'
+
+
+class Bracket(Token):
+    def __init__(self, side):
+        super().__init__()
+        self.type = 'bracket'
+        self.side = side
+
+
+class LeftBracket(Bracket):
+    def __init__(self):
+        super().__init__(side='L')
+        self.char = '['
+
+
+class RightBracket(Bracket):
+    def __init__(self):
+        super().__init__(side='R')
+        self.char = ']'
+
+
+class Quantifier(Token):
+    def __init__(self, quantity, qtifier_char):
+        super().__init__()
+        self.type = 'qtifier'
+        self.quantity = quantity
+        self.qtifier_char = qtifier_char
+        self.char = qtifier_char
+
+
+class ZeroOrMore(Quantifier):
+    def __init__(self, qtifier_char):
+        super().__init__(quantity='zeroOrMore', qtifier_char=qtifier_char)
+
+
+class OneOrMore(Quantifier):
+    def __init__(self, qtifier_char):
+        super().__init__(quantity='oneOrMore', qtifier_char=qtifier_char)
+
+
+class ZeroOrOne(Quantifier):
+    def __init__(self, qtifier_char):
+        super().__init__(quantity='zeroOrOne', qtifier_char=qtifier_char)
+
+
+class Asterisk(ZeroOrMore):
+    def __init__(self):
+        super().__init__('*')
+
+
+class Plus(OneOrMore):
+    def __init__(self):
+        super().__init__('+')
+
+
+class QuestionMark(ZeroOrOne):
+    def __init__(self):
+        super().__init__('?')
+
+
+class OrToken(Token):
+    def __init__(self, or_ch):
+        super().__init__()
+        self.type = 'or'
+        self.or_ch = or_ch
+        self.char = or_ch
+
+
+class VerticalBar(OrToken):
+    def __init__(self):
+        super().__init__('|')
+
+
+class NotToken(Token):
+    def __init__(self, not_ch):
+        super().__init__()
+        self.type = 'not'
+        self.not_ch = not_ch
+        self.char = not_ch
+
+
+class Circumflex(NotToken):
+    def __init__(self):
+        super().__init__('^')
+
+
+class Dash(Token):
+    def __init__(self):
+        super().__init__()
+        self.type = 'dash'
+        self.ch = '-'
+        self.char = '-'
+
+token_dict = {
+    '.': Wildcard,
+    '-': Dash,
+    '^': Circumflex,
+    '|': VerticalBar,
+    '?': QuestionMark,
+    '+': Plus,
+    '*': Asterisk,
+    ']': RightBracket,
+    '[': LeftBracket,
+    '{': LeftCurlyBrace,
+    '}': RightCurlyBrace,
+    '(': LeftParenthesis,
+    ')': RightParenthesis,
+    ',': Comma, 
+    '\\': Escape,
+    '$' : End,
+    '~': Start,
+}
