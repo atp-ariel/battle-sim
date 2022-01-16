18c67df49b2224a1350cacbaba17b95b34896443
CTS-crypto <72420685+CTS-crypto@users.noreply.github.com>
2022-01-16 18:21:14 -0500

    tabala

--- src/language/grammar/parser.py
+++ src/language/grammar/parser.py
@@ -1,5 +1,6 @@
 from grammar import *
 from queue import deque
+import json
 
 class Item:
     def __init__(self,production:Production,index):
@@ -49,6 +50,7 @@ class State:
         self.items=set(kernel)
         self.nexts={}
         self.expected_symbols={}
+        self.number=0
         
         for item in kernel:
             self.string+=f"{item} |"
@@ -56,7 +58,7 @@ class State:
         self.hash=hash(self.string)
     
     def __hash__(self):
-        return hash(self.string)
+        return self.hash
     
     def __eq__(self, o):
         if isinstance(o, State):
@@ -95,7 +97,7 @@ class State:
                         self.add_item(new_item)
                         q.append(new_item)
                     
-    def go_to(self,sym:Symbol,states,q:deque,initial_items):
+    def go_to(self,sym:Symbol,states_set,state_list,q:deque,initial_items):
         new_kernel=[]
         
         for i in self.expected_symbols[sym]:
@@ -103,14 +105,15 @@ class State:
             new_kernel.append(new_item)
         new_state=State(new_kernel)
         
-        if new_state not in states:
+        if new_state not in states_set:
+            new_state.number=len(states_set)
             new_state.build(initial_items)
-            states.add(new_state)
+            states_set.add(new_state)
+            state_list.append(new_state)
             q.append(new_state)
         
         self.nexts[sym]=new_state
-            
-        
+                    
 class Automaton:
     
     def __init__(self, grammar:Grammar):
@@ -130,17 +133,60 @@ class Automaton:
                 
         i0=State(initial_items[s])
         
-        states=set([i0])
-        q=deque([i0])
+        states_list=[i0]
+        states_set=set(states_list)
+        q=deque(states_list)
            
         while len(q)!=0:
             state=q.popleft()
             state.build(initial_items)
             for sym in state.expected_symbols:
-                state.go_to(sym, states,q,initial_items)
+                state.go_to(sym, states_set,states_list,q,initial_items)
                 
-        return states            
+        return states_list            
                 
+
+class TableActionGoTo:
+    def __init__(self,grammar:Grammar):
+        self.grammar=grammar
+        
+    def build(self):
+        
+        states=Automaton(self.grammar).build()
+        
+        action=[]
+        go_to=[]
+        
+        for state in states:
+            state_action={}
+            state_go_to={}
+            
+            for n in state.nexts:
+                if n.is_terminal:
+                    state_action[n.name]=('S',state.nexts[n].number)
+                else:
+                    state_go_to[n.name]=state.nexts[n].number
+            
+            lookaheads={}
+            for i in state.items:
+                if i.index==len(i.production):
+                    if i.lookahead in lookaheads:
+                        raise Exception('Conflicto Reduce-Reduce')
+                    lookaheads[i.lookahead]=i
+                    
+            for l in lookaheads:
+                if l in state_action:
+                    raise Exception('Conflicto Shift-Reduce')
+                state_action[l.name]=('R',i.production.head.name,len(i.production))
+                
+            action.append(state_action)
+            go_to.append(state_go_to)
+        
+        with open('action.json','w') as fout:
+            json.dump(action, fout)
+            
+        with open('go_to.json','w') as fout:
+            json.dump(go_to, fout)
 
 i=Terminal("i") 
 plus=Terminal("+")
@@ -160,6 +206,6 @@ A+=Production([i])
 
 g=Grammar([E,A])
 
-a=Automaton(g)
 
-print(len(a.build()))
+table=TableActionGoTo(g)
+table.build()
