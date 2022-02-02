47ed762f820bb523d98db31f94649eb384391e2b
ArielTriana <usich37@gmail.com>
2022-02-02 11:06:03 -0500

    Fix tokens test

--- test/test_simulator.py
+++ test/test_simulator.py
@@ -20,25 +20,24 @@ def test_simulator():
     sea = 0.45
     n = 50
     maps = GAT_Generator(.95, (n, n), sea_level=sea)()
-    maps.get_img().show()
     maps = LandMap(n, n, maps, sea, [])
 
     # Side configuration
-    s1 = Side(1, "s1")
-    s2 = Side(2, "s2")
+    s1 = Side(1, [])
+    s2 = Side(2, [])
 
 
     # Units creation
     for i in range(10):
-        s1.add_unit(LandUnit(i+1,r(1,10),r(1,10),s1,r(1,10),r(1,10),r(1,10),r(1,10),r(1,1),r(1,2),1,r(1,10),0,True,True))
+        s1.add_unit(LandUnit(i+1,r(1,10),r(1,10),r(1,10),r(1,10),r(1,10),r(1,10),r(1,1),r(1,2),1,r(1,10),0,True,True))
         s1.units[-1].put_in_cell(maps, 0, i)
 
     for i in range(10):
-        s2.add_unit(LandUnit(i+10+1,r(1,10),r(1,10),s2,r(1,10),r(1,10),r(1,10),r(1,10),r(1,1),r(1,2),1,r(1,10),0,True,True))
+        s2.add_unit(LandUnit(i+10+1,r(1,10),r(1,10),r(1,10),r(1,10),r(1,10),r(1,10),r(1,1),r(1,2),1,r(1,10),0,True,True))
         s2.units[-1].put_in_cell(maps, n-1, i)
 
 
-    S = Simulator(maps, maps, [s1, s2], 10)
+    S = Simulator(maps, [s1, s2], 10)
 
     S.simulator_by_turns()
 
