b23d6081a2271ef359db557a15314c57387f38c2
Roci <56322127+rocioog00@users.noreply.github.com>
2022-02-06 02:34:17 -0500

    Add files via upload

--- src/language/semantic_analysis/type_checker.py
+++ src/language/semantic_analysis/type_checker.py
@@ -7,7 +7,6 @@ from ..parser.ast import *
 class Type_Checker:
     def __init__(self, context):
         self.context = context
-        self.var = []
 
     @visitor(BsFile)
     def visit(self, node: BsFile, logger):
@@ -16,6 +15,95 @@ class Type_Checker:
         
         for s in node.statements:
             self.visit(s, logger)
+
+    @visitor(ClassDef)
+    def visit(self,node:ClassDef,logger):
+        for a in node.attributes:
+            self.visit(a,logger)
+
+        for m in node.methods:
+            self.visit(m,logger)
+
+
+        node.computed_type="Class"
+
+    @visitor(AttrDef)
+    def visit(self,node:AttrDef,logger):
+        self.visit(node.init,logger)
+        node.computed_type=node.init.computed_type
+
+    @visitor(FuncDef)
+    def visit(self,node:FuncDef,logger):
+        for i in node.body:
+            self.visit(i)
+
+        node.computed_type=node.return_type
+
+        
+
+    @visitor(If)
+    def visit(self,node:If,logger):
+        self.visit(node.condition,logger)
+
+        for i in node.body:
+            self.visit(i,logger)
+
+        node.computed_type=None
+
+    @visitor(Branch)
+    def visit(self,node:Branch,logger):
+        for i in node.ifs:
+            self.visit(i,logger)
+
+        for e in node.else_body:
+            self.visit(e,logger)
+        
+        node.computed_type=None
+
+
+    @visitor(WhileDef)
+    def visit(self,node:WhileDef,logger):
+        self.visit(node.condition,logger)
+
+        for i in node.body:
+            self.visit(i,logger)
+
+        node.computed_type=None
+        
+
+    @visitor(Decl)
+    def visit(self,node:Decl,logger):
+        self.visit(node.expression)
+        if self.context.check_var_type(node.name,node.type) and node.expression.computed_type == node.type:
+            node.computed_type=None
+
+        else:
+            logger.error("Type mismatch...")
+            node.computed_type = None
+            
+    @visitor(Assign)
+    def visit(self,node:Assign,logger):
+        self.visit(node.expression)
+        if self.context.check_var_type(node.name,node.expression.computed_type):
+            node.computed_type=None
+
+        else:
+            logger.error("Type mismatch...")
+            node.computed_type = None
+
+    @visitor(Return)
+    def visit(self,node:Return,logger):
+        self.visit(node.expression)
+
+        node.computed_type=None
+
+    @visitor(Break)
+    def visit(self,node:Break,logger):
+        node.computed_type=None
+
+    @visitor(Continue)
+    def visit(self,node:Continue,logger):
+        node.computed_type=None
 
     @visitor(BinaryExpression)
     def visit(self, node: BinaryExpression, logger):
@@ -27,14 +115,8 @@ class Type_Checker:
         node.computed_type = node.right.computed_type
 
         if node.left.computed_type != node.right.computed_type:
-            if node.left.computed_type == "Var":
-                node.computed_type = node.left.computed_type
-                var = self.var.pop()
-                self.context.define_var(var, node.computed_type)
-
-            else:
-                logger.error("Type mismatch...")
-                node.computed_type = None
+            logger.error("Type mismatch...")
+            node.computed_type = None
 
         else:
             node.computed_type = node.left.computed_type
@@ -92,7 +174,7 @@ class Type_Checker:
                 node.computed_type = _type.get_attribute(node.name)[0]
 
         else:
-            if node.expression.computed_type == "Var":
+            if node.expression.computed_type == "function":
                 args = [0]*len(node.args)
 
                 for i in range(len(node.args)):
@@ -110,10 +192,8 @@ class Type_Checker:
             node.computed_type = self.context.get_type(node.name)
 
         else:
-            self.var.append(node.name)
-            node.computed_type = "Var"
-        pass
-
+            logger.error(f"name {node.name} is not defined")
+            
     @visitor(Number)
     def visit(self, node: Number, logger):
         node.computed_type = "Number"
